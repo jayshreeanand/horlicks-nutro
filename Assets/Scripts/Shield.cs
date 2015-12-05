93f22b37775626232a1c4a0435b49eb0a8998ddd
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2015-12-06 00:32:27 +0530

    Protect user in Shield mode

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class Shield : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	 void OnTriggerEnter2D(Collider2D other) {
+    if(other.tag == "Enemy") {
+      Destroy(other);
+    }
+  }
 }
